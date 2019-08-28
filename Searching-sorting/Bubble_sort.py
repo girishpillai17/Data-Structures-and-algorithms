54a7860760ea48d4ae360c3fc11c663677549546
Girish <girishpillai239@gmail.com>
2019-08-28 15:32:50 +0530

    bubble sort

---
+++ Searching-sorting/Bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(array):
+    n = len(array)
+
+    for i in range(n-1,0,-1):
+
+        for j in range(i):
+
+            if array[j] > array[j+1]:
+
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+
+    return array
+
+arr = [10,9,8,7,6,5,4,3,2,1]
+print(bubble_sort(arr))
---
+++ test.py
@@ -0,0 +1,4 @@
+#snippet to test some code for practice
+
+n = list(range(9,0,-1))
+print(n)
