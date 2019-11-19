d129695f18013d863718fdec9993ad0f34c0c01a
Girish <girishpillai239@gmail.com>
2019-11-19 14:24:18 +0530

    test

--- Hackerrank/Apples_and_oranges.py
+++ Hackerrank/Apples_and_oranges.py
@@ -22,27 +22,19 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         else:
             countb == countb
         
-    return (counta, countb)
+    print(counta)
+    print(countb)
 
 if __name__ == '__main__':
     st = input().split()
-
     s = int(st[0])
-
     t = int(st[1])
-
     ab = input().split()
-
     a = int(ab[0])
-
     b = int(ab[1])
-
     mn = input().split()
-
     m = int(mn[0])
-
     n = int(mn[1])
-
     apples = list(map(int, input().rstrip().split()))
 
     oranges = list(map(int, input().rstrip().split()))
